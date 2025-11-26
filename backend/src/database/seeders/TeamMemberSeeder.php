b392f6da156c921f6c5ccb4b8781b5b8a529e549
Kevin Ponce <kevin.ponce@ct.uneatlantico.es>
2025-11-26 14:13:29 +0100

    feat: Populate TeamMemberSeeder with initial team member data

--- backend/src/database/seeders/TeamMemberSeeder.php
+++ backend/src/database/seeders/TeamMemberSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TeamMember;
 
 class TeamMemberSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class TeamMemberSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $teamMembers = [
+            ['name' => 'pipe', 'role' => 'Frontend/Sysadmin'],
+            ['name' => 'carlos', 'role' => 'Sysadmin'],
+            ['name' => 'moys', 'role' => 'SL'],
+            ['name' => 'brayan', 'role' => 'Frontend Líder'],
+            ['name' => 'jimenez', 'role' => 'Backend Líder'],
+            ['name' => 'angel', 'role' => 'Frontend'],
+            ['name' => 'katerine', 'role' => 'BA'],
+            ['name' => 'kevin', 'role' => 'Backend'],
+            ['name' => 'marco', 'role' => 'Backend/QA/DBA'],
+        ];
+
+        foreach ($teamMembers as $member) {
+            TeamMember::create($member);
+        }
     }
 }
